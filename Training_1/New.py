01db20cce7437a22af2426e6ce42ee3d2acf36a2
tristandewildt <titan946@gmail.com>
2016-04-24 14:14:41 +0200

    Initial

---
+++ Training_1/New.py
@@ -0,0 +1,37 @@
+'''
+Created on 17 apr. 2016
+
+@author: Tristan
+'''
+#from cmath import pi
+import math
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+    radius = 5
+    sphere = (4/3) * math.pi * (radius**3) 
+    
+    print(sphere)
+    decibels = 10*math.log(sphere)
+    print(decibels)
+    
+    
+
+    def print_twice(self):
+        print(self)
+        print(self)
+    
+    def cat_twice(self, x, y):
+        cat = x + y
+        print(cat)
+        
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
+    print_twice('Pieter')
+    cat_twice('o', '2', '1')
+    
