7938e66203b9ddc8a1e50a80298e76b138f4d73d
tristandewildt <tewdewildt@tudelft.net>
2016-08-05 16:29:45 +0200

    Chap 14

--- Training_1/chap14.py
+++ Training_1/chap14.py
@@ -76,30 +76,88 @@ t2 = pickle.loads(s)
 #stat = fp.close()
 #print(stat)
 
-filename = 'de_wildt_phd_literature_review_3.tex'
-cmd = 'md5sum ' + filename
-fp = os.popen(cmd)
-res = fp.read()
-stat = fp.close()
-print(res)
-print(stat)
+#filename = 'de_wildt_phd_literature_review_3.tex'
+#cmd = 'md5sum ' + filename
+#fp = os.popen(cmd)
+#res = fp.read()
+#stat = fp.close()
+#print(res)
+#print(stat)
 
+def linecount(filename):
+    count = 0
+    for line in open(filename):
+        count += 1
+    return count
 
+#print(linecount('words.txt'))
 
+#if __name__ == '__main__': #if the program is running as a script, __name_ has the value '__main__'. If the module is being imported, the test code is skipped.
+#    print(linecount('words.txt'))
 
+s = '1 2\t 3\n 4'
+#print(s)
+#print(repr(s))
 
+def sed(pat_string, repl_string, file1, file2):
+    try:
+        fin = open(file1)
+    except:
+        print('Something went wrong, could not open the file.')
+        return
+    
+    temp = []
+    for line in fin:
+        print(line)
+        print(pat_string)
+        if str(line) == str(pat_string):
+            line == repl_string
+        temp.append(line.strip())
+    fin.close
+    
+    db = dbm.open(file2, 'c')
+    for value in temp:
+        db[value]= str(1)
+    db.close()
+    
+    
+    #return temp
 
+pat_string = 'Lilili'
 
+repl_string = 'Lululu'
 
+file1 = 'file1.txt'
+file2 = 'replaced.' + file1 
+#sed(pat_string, repl_string, file1, file2)
 
+"""
+# Improved code is
+"""
+def main():
+    pat_string2 = 'Lilili'
+    repl_string2 = 'Lululu'
+    file3 = 'file1.txt'
+    file4 = 'replaced.' + file1 
+    sed2(pat_string2, repl_string2, file3, file4)
 
+def sed2(pat_string, repl_string, file1, file2):
+    fin = open(file1, 'r')
+    dump = open(file2, 'w')
+    for line in fin:
+        line = line.replace(pat_string, repl_string)
+        dump.write(line)
+    fin.close()
+    dump.close()
+#main()
 
+""" 
+Exercise 14-2
+"""
 
-
-
-
-
-
-
-
+def exo14_2():
+    from chap12 import search_anagrams
+    x = search_anagrams()
+    print(x)
+exo14_2()
 
