7f2b42ad9799f5d1d972ca7380ccbaa70d78cba9
tristandewildt <tewdewildt@tudelft.net>
2016-09-20 15:51:11 +0200

    Chap15-2

--- Training_1/chap15.py
+++ Training_1/chap15.py
@@ -4,13 +4,99 @@ Created on 17 apr. 2016
 @author: Tristan
 '''
 #from cmath import pi
-import math, operator
-import os
-import dbm
-import pickle
-from xlwt.antlr import ifelse
-from tabulate import tabulate
-from sympy.core.numbers import NumberSymbol
-from _ast import Str
+import math
+import copy
+
+
+class Point:
+    """Represents a point in 2-D space."""
+
+blank = Point()
+blank.x = 9
+blank.y = 10
+
+black = Point()
+black.x = 7
+black.y = 2
+
+#print(blank.y)
+
+# Here Point is an object, blank is a variable and both y and z are attributes. Each attribute refers to a floating-point number.
+
+def print_point(p):
+    print('(%g, %g)' % (p.x, p.y))
+
+#print_point(blank)
+    
+
+def distance_between_points(a,b):
+    distance = math.sqrt((b.x-a.x)**2 + (b.y-a.y)**2)
+    return distance
+
+#print(distance_between_points(blank, black))
+
+
+class Rectangle:
+    """Represents a rectangle
+    attributes: width, height, corner.
+    """
+
+box = Rectangle()
+box.width = 100.0
+box.height = 200.0
+box.corner = Point()
+box.corner.x = 0.0
+box.corner.y = 0.0
+
+def find_center(rect):
+    p = Point()
+    p.x = (rect.width + rect.corner.x) /2
+    p.y = (rect.height + rect.corner.y) /2
+    return p
+
+center = find_center(box)
+#print_point(center)
+
+def move_rectangle(rect, dx, dy):
+    rect.corner.x += dx
+    rect.corner.y += dy
+    
+move_rectangle(box, 100, 200)
+center = find_center(box)
+#print_point(center)
+
+"""
+Copying
+"""
+p1=Point()
+p1.x=1
+p1.y=2
+
+p2 = copy.copy(p1)
+#print_point(p2)
+
+# So p2 has now the same coordinates as p1, but is not the same point
+
+# When you copy, you're goal is not to to create new object, but have the same values as the one copied. A good copy return false for box2 = box
+# shallow copy: e.g. when you perform box2=copy.copy(box), it copies the objects and the attributes, but not the embedded objects.
+# In most cases, this is not what you want.
+# so you may use 'deepcopy', which also copies objects within objects.
+
+box2=copy.copy(box)
+print(box2 is box, box2.corner is box.corner)
+
+box3=copy.deepcopy(box) 
+print(box3 is box, box3.corner is box.corner)
+
+
+
+
+
+
+
+
+
+
+
 
 
